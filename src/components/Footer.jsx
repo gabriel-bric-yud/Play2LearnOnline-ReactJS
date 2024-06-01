f8906d8bef0470fa81cee031d6889931585763d9
gbabyblue <gabrielyudken@gmail.com>
2024-06-01 02:03:51 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="navbar fixed-bottom bg-primary">
       <div className="container-fluid">
         <a href="https://github.com/gbabyblue" className="nav-link text-light">
-          {new Date().getFullYear()} - Gabriel Yudken
+          {new Date().getFullYear()} <a className="text-light" href = "https://www.ed2go.com">ed2go</a> - Gabriel (github)
         </a>
       </div>
     </footer>
